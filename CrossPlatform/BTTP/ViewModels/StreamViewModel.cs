e96e8c43a532890d841e566c1a907c602cdce052
Thomas Denney <me@thomasdenney.co.uk>
2017-04-26 16:52:26 +0100

    Simplify property observation

--- CrossPlatform/BTTP/ViewModels/StreamViewModel.cs
+++ CrossPlatform/BTTP/ViewModels/StreamViewModel.cs
@@ -5,40 +5,22 @@ using System.Diagnostics;
 
 namespace BTTP
 {
-    public abstract class StreamViewModel : INotifyPropertyChanged
+    public abstract class StreamViewModel : ObservableObject
     {
         public string Title { get; protected set; }
 
         private List<ItemViewModel> items;
         public List<ItemViewModel> Items
         {
-            get
-            {
-                return items;
-            }
-
-            protected set
-            {
-                items = value;
-                OnPropertyChanged("Items");
-            }
+            get { return items; }
+            protected set { SetProperty(ref items, value, "Items"); }
         }
 
         public bool IsBusy { get; set; }
 
         public virtual void UpdateModel(AllData data)
         {
-            Debug.WriteLine("Update model");
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected void OnPropertyChanged(string name)
-        {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(name));
-            }
+            // Implemented by subclasses
         }
     }
 }
