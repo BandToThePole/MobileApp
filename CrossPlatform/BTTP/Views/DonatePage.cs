ee9704e9a3e0c10a6fb0fdf1bff7f59ab6f6112f
Alexandru Văleanu <valeanu_alex@yahoo.com>
2017-04-27 09:58:01 +0100

    add donate and twitter pages

---
+++ CrossPlatform/BTTP/Views/DonatePage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace BTTP
+{
+    public class DonatePage : ContentPage
+    {
+        public DonatePage()
+        {
+            this.Title = "Donate!";
+
+            Content = new StackLayout
+            {
+                Children = {
+                    new WebView()
+                    {
+                        Source = "http://uk.virginmoneygiving.com/JamesRedden",
+                        VerticalOptions = LayoutOptions.FillAndExpand,
+                        HorizontalOptions = LayoutOptions.FillAndExpand
+                    }
+                }
+            };
+        }
+    }
+}
---
+++ CrossPlatform/BTTP/Views/TwitterPage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace BTTP
+{
+    public class TwitterPage : ContentPage
+    {
+        public TwitterPage()
+        {
+            this.Title = "Twitter";
+            
+            Content = new StackLayout
+            {
+                Children = {
+                    new WebView()
+                    {
+                        Source = "https://twitter.com/jredden",
+                        VerticalOptions = LayoutOptions.FillAndExpand,
+                        HorizontalOptions = LayoutOptions.FillAndExpand
+                    }
+                }
+            };
+        }
+    }
+}
